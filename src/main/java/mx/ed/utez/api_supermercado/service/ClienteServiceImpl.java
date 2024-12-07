7b84b216b129527ca2ecda028ddb57004d8e557b
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-07 02:12:10 -0600

    creacion de ClienteServeceImpl

--- src/main/java/mx/ed/utez/api_supermercado/service/ClienteServiceImpl.java
+++ src/main/java/mx/ed/utez/api_supermercado/service/ClienteServiceImpl.java
@@ -1,4 +1,18 @@
 package mx.ed.utez.api_supermercado.service;
 
-public class ClienteServiceImpl {
+import mx.ed.utez.api_supermercado.model.Cliente;
+import mx.ed.utez.api_supermercado.model.dao.IClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @Override
+    public Cliente guardarCliente(Cliente cliente) {
+        return clienteDao.save(cliente);
+    }
 }
