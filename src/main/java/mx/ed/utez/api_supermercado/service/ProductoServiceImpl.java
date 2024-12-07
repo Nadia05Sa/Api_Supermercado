92d4de752cf158a59b1774f4ba52eeff3967fde3
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-07 01:38:25 -0600

    creacion de ProductoServiceImpl

--- src/main/java/mx/ed/utez/api_supermercado/service/ProductoServiceImpl.java
+++ src/main/java/mx/ed/utez/api_supermercado/service/ProductoServiceImpl.java
@@ -1,4 +1,18 @@
 package mx.ed.utez.api_supermercado.service;
 
-public class ProductoServiceImpl {
+import mx.ed.utez.api_supermercado.model.Producto;
+import mx.ed.utez.api_supermercado.model.dao.IProductoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoServiceImpl implements IProductoService{
+
+    @Autowired
+    private IProductoDao productoDao;
+
+    @Override
+    public Producto guardar(Producto producto){
+        return productoDao.save(producto);
+    }
 }
