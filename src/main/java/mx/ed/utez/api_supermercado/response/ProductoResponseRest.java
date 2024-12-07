bdc8f3c0c324cb88dfb600a364f6ac2c3553dac2
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-07 00:47:22 -0600

    creacion de la clase ProductoResponseRest

--- src/main/java/mx/ed/utez/api_supermercado/response/ProductoResponseRest.java
+++ src/main/java/mx/ed/utez/api_supermercado/response/ProductoResponseRest.java
@@ -1,4 +1,14 @@
 package mx.ed.utez.api_supermercado.response;
 
-public class ProductoResponseRest {
+public class ProductoResponseRest extends ProductoResponse {
+
+    private ProductoResponse productoResponse = new ProductoResponseRest();
+
+    public ProductoResponse getProductoResponse() {
+        return productoResponse;
+    }
+
+    public void setProductoResponse(ProductoResponse productoResponse) {
+        this.productoResponse = productoResponse;
+    }
 }
