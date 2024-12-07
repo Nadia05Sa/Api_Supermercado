fc2d56faec2025b22dbebb2ec316900fc73b5057
rubengo <20233tn207@utez.edu.mx>
2024-12-07 00:23:46 -0600

    Agregue Producto dao

--- src/main/java/mx/ed/utez/api_supermercado/model/dao/IProductoDao.java
+++ src/main/java/mx/ed/utez/api_supermercado/model/dao/IProductoDao.java
@@ -1,4 +1,16 @@
 package mx.ed.utez.api_supermercado.model.dao;
 
-public interface IProductoDao {
+import mx.ed.utez.api_supermercado.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IProductoDao extends JpaRepository<Producto, Long> {
+    // Métodos personalizados
+
+    // Buscar productos por nombre (coincidencia exacta)
+    Producto findByNombre(String nombre);
+
 }
