c347a6503ded1b96575e3f62da94a0d190cac022
rubengo <20233tn207@utez.edu.mx>
2024-12-07 00:18:14 -0600

    Agregue Carrito dao

--- src/main/java/mx/ed/utez/api_supermercado/model/dao/ICarritoDao.java
+++ src/main/java/mx/ed/utez/api_supermercado/model/dao/ICarritoDao.java
@@ -1,4 +1,14 @@
 package mx.ed.utez.api_supermercado.model.dao;
 
-public interface ICarritoDao {
+import mx.ed.utez.api_supermercado.model.CarritoProducto;
+import mx.ed.utez.api_supermercado.model.CarritoProducto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICarritoDao extends JpaRepository<CarritoProducto, Long> {
+    // Si necesitas buscar carritos por cliente, puedes agregar métodos personalizados.
+
+    // Ejemplo: Buscar un carrito por cliente
+    CarritoProducto findByClienteId(Long clienteId);
 }
