75bce7ae00f514b1ccf2b13f33f9685eceb55603
rubengo <20233tn207@utez.edu.mx>
2024-12-07 00:12:52 -0600

    Agregue Cliente dao

--- src/main/java/mx/ed/utez/api_supermercado/model/dao/IClienteDao.java
+++ src/main/java/mx/ed/utez/api_supermercado/model/dao/IClienteDao.java
@@ -1,4 +1,10 @@
 package mx.ed.utez.api_supermercado.model.dao;
 
-public interface IClienteDao {
+import mx.ed.utez.api_supermercado.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
+    // Métodos adicionales si son necesarios
 }
