55602fe5af990a24dc56bdbb7f9bfc57debee95c
Nadia05Sa <20233tn237@utez.edu.mx>
2024-12-07 01:22:22 -0600

    creacion de IProductorService

--- src/main/java/mx/ed/utez/api_supermercado/service/IProductoService.java
+++ src/main/java/mx/ed/utez/api_supermercado/service/IProductoService.java
@@ -1,4 +1,7 @@
 package mx.ed.utez.api_supermercado.service;
 
+import mx.ed.utez.api_supermercado.model.Producto;
+
 public interface IProductoService {
+    Producto guardar(Producto producto);
 }
